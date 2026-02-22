08efea73e37fdccfdd558cce8b9df8a2995d83c0
Alexej Bykov <bykov.svs@gmail.com>
2026-02-22 11:05:21 +0100

    Add dotenv author printing

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+
 print('Hello from repository!')
+from dotenv import load_dotenv
+import os
+
+
+def print_author():
+    load_dotenv()  # загружает переменные из .env
+    author = os.getenv("AUTHOR")  # читаем AUTHOR из .env
+    print(f"Автор проекта: {author}")
+
+
+print_author()
